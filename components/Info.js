1a1f71315fae0af24322982d77d83103467633fb
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-09-03 20:42:50 -0300

    Styled the Info.js file

--- components/Info.js
+++ components/Info.js
@@ -1,47 +1,64 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Image from "next/image";
+import styles from "../styles/Info.module.css";
 import wallets from "../public/wallets.png";
 import cards from "../public/cards.png";
 import album from "../public/album.png";
 import { glowOnHover } from "../styles/Hero.module.css";
 
+const { bloque, bloqueTexto, reverse, bloqueImg } = styles;
+
 export default function Info() {
   return (
     <section>
-      <div>
-        <h3>¿Qué es NFT Neo FuTbol?</h3>
-        <p>Bienvenide al primer album digital de figuritas del Mundial Qatar 2022! 
-            Neo FuTbol te permite coleccionar NFTs de los jugadores participantes en el Mundial de futbol de Qatar 2022, 
+      <div className={bloque}>
+        <div className={bloqueTexto}>
+          <h3>¿Qué es NFT Neo FuTbol?</h3>
+          <p>Bienvenide al primer album digital de figuritas del Mundial Qatar 2022! 
+            <br />
+            <b>Neo FuTbol</b> te permite coleccionar NFTs de los jugadores participantes en el Mundial de futbol de Qatar 2022,
             cada figurita tiene su propia rareza, no solo por la cantidad de figuritas disponibles sino también por su Tier:
             <ul>
-              <li>Común: figurita de cuerpo dorado y sin decoración en el marco.</li>
-              <li>Heroico: figurita de cuerpo azulado y marco con decoraciones doradas.</li>
-              <li>Legendario: figurita de cuerpo platinado y marco con decoraciones doradas y platinadas.</li>
+              <li>Tier Común: figurita de cuerpo dorado y sin decoración en el marco.</li>
+              <li>Tier Heroico: figurita de cuerpo azulado y marco con decoraciones doradas.</li>
+              <li>Tier Legendario: figurita de cuerpo platinado y marco con decoraciones doradas y platinadas.</li>
             </ul>
-            </p>
-        <Image src={cards}></Image>
+          </p>
+        </div>
+        <div>
+          <Image src={cards} className={bloqueImg}></Image>
+        </div>
       </div>
-      <div>
-        <h3>Ganar, ganar y ganar!</h3>
-        <p>No solo puedes ganar obteniendo figuritas difíciles, también puedes ganar con su Tier y, ademas, si eres la 
-          primer persona en completar el album al 100% ganas un premio especial de 2 Ethers!
-        </p>
-        <Image src={album}></Image>
+      <div className={[bloque, reverse].join(" ")}>
+        <div className={bloqueTexto}>
+          <h3>Ganar, ganar y ganar!</h3>
+          <p>No solo puedes ganar obteniendo figuritas difíciles, también puedes ganar con su Tier y, ademas, si eres la
+            primer persona en completar el album al 100% ganas un premio especial de 2 Ethers!
+          </p>
+        </div>
+        <div>
+          <Image src={album} className={bloqueImg}></Image>
+        </div>
       </div>
-      <div>
-        <h3>Comenzá...</h3>
-        <p>Comenzar a ser dueño de tu pasión es fácil:
-          <ul>
-            <li>Conecta tu wallet</li>
-            <li>Compra tu primera figurita</li>
-            <li>Comparte tu pasión</li>
-          </ul>
-        </p>
-        <Image src={wallets}></Image>
+      <div className={bloque}>
+        <div className={bloqueTexto}>
+          <h3>Comenzá...</h3>
+          <p>Comenzar a ser dueño de tu pasión es fácil:
+            <ul>
+              <li>Conecta tu wallet</li>
+              <li>Compra tus primeras figuritas</li>
+              <li>Compartí tu pasión!</li>
+            </ul>
+          </p>
+        </div>
+        <div>
+          <Image src={wallets} className={bloqueImg}></Image>
+        </div>
       </div>
-      
-      <Button className={glowOnHover} variant="danger" size="lg">Conecta tu wallet</Button>{' '}
+      <div className={bloque}>
+        <Button className={glowOnHover} variant="danger" size="lg">Conecta tu wallet</Button>{' '}
+      </div>
     </section>
   )
 }
