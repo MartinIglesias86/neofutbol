0d6ae4f6093a2ea601735947ca938f17082d2031
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-09-03 17:24:03 -0300

    Added Footer component

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { footer } from "../styles/Footer.module.css";
+
+export default function Footer() {
+  return(
+    <div className={footer}>
+      <p>Made with <span role="img" aria-label="love">💜</span> by NFT Neo FuTbol</p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Header />
         <Hero />
         <div>Hola</div>
+        <Footer />
       </main>
     </div>
   )
